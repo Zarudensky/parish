ebdcb9168ec8f2c215d3ab38d3983a05178e1598
Stas Zarudensky <szarudensky@gmail.com>
2020-03-12 13:15:36 +0200

    Add learn modal

--- js/main.js
+++ js/main.js
@@ -18,12 +18,24 @@ $(document).ready(function(){
 		$('.header__block_menu').addClass('active');
 		$('.menu__back').addClass('active');
 		$('.header__block_menu').removeClass('closed');
+		$('body').addClass('fixed');
 	});
 	$('.menu__btn_back, .header__block_menu, .menu__back').click(function() {
 		if ($(window).width() < 1220) {
 			$('.header__block_menu').removeClass('active');
 			$('.menu__back').removeClass('active');
 			$('.header__block_menu').addClass('closed');
+			$('body').removeClass('fixed');
 		}
+	});
+
+	// modal form
+	$('#learnMore').click(function() {
+		$('.modal').addClass('active');
+	  	$('body').addClass('overflow');
+	});
+	$('.close, .modal__back').click(function() {
+	  	$('.modal').removeClass('active');
+		$('body').removeClass('overflow');
 	});
 });
